0574acd790a89e63f4baaf235f56caa61f88fb66
yhromovyi <yaroslav.hromovyi@vonage.com>
2024-10-13 19:33:34 +0200

    Initial commit

---
+++ app.vue
@@ -0,0 +1,25 @@
+<script lang="ts" setup>
+useHead({
+  title: 'Thunder',
+  titleTemplate: getTitleTemplate,
+  meta: [
+    {
+      name: 'description',
+      content: 'Platform agnostic headless storefront'
+    }
+  ]
+});
+</script>
+
+<template>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
+</template>
+
+<style lang="postcss">
+.page-enter-active,
+.page-leave-active {
+  @apply transition-all ease-linear;
+}
+</style>
---
+++ nuxt.config.ts
@@ -0,0 +1,16 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+import dotenv from 'dotenv'
+
+dotenv.config();
+
+export default defineNuxtConfig({
+  compatibilityDate: '2024-04-03',
+
+  extends: [
+    ['github:gromovoy88/nuxt-commerce/packages/api-mock', { auth: process.env.GITHUB_TOKEN }],
+    ['github:gromovoy88/nuxt-commerce/packages/sdk', { auth: process.env.GITHUB_TOKEN }],
+    ['github:gromovoy88/nuxt-commerce/packages/thunder', { auth: process.env.GITHUB_TOKEN }],
+  ],
+
+  devtools: { enabled: true }
+})
